caeebcd35c077167eb6e2a6c0728bc3d936d12d3
Daniel Tael <daniel@tael.nu>
2017-04-27 16:44:04 +0200

    Adding files
    Adding files

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+;(function( $ ){
+
+	/**
+	 * In genereal you should avoid to use jQuery code in AngularJS
+	 * apps, if you need any jQuery functionality create a directive
+	 *
+	 */
+  $(document).ready(function() {
+
+  });
+
+  $(window).load(function() {
+    Materialize.updateTextFields();
+    $(".button-collapse").sideNav();
+  });
+
+
+})( jQuery );
